5dfbc598faf0473d7e5214f3c44ea1e92d6613ed
Robert Gleason <l2ob122@gmail.com>
2024-06-03 21:27:33 -0700

    chore: Add signup route handler and view template

--- handler/auth.go
+++ handler/auth.go
@@ -4,34 +4,51 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/epiq122/epiqpixai/pkg/kit/validate"
 	"github.com/epiq122/epiqpixai/pkg/sb"
-	"github.com/epiq122/epiqpixai/pkg/util"
 	"github.com/epiq122/epiqpixai/view/auth"
 
 	"github.com/nedpals/supabase-go"
 )
 
 func HandleLoginIndex(w http.ResponseWriter, r *http.Request) error {
-	return auth.Login().Render(r.Context(), w)
+	return render(r, w, auth.Login())
 
+}
+
+func HandleSignupIndex(w http.ResponseWriter, r *http.Request) error {
+	return render(r, w, auth.Signup())
+
+}
+
+func HandleSignupCreate(w http.ResponseWriter, r *http.Request) error {
+	params := auth.SignupParams{
+		Email:           r.FormValue("email"),
+		Password:        r.FormValue("password"),
+		ConfirmPassword: r.FormValue("confirmPassword"),
+	}
+	errors := auth.SignupErrors{}
+	if ok := validate.New(&params, validate.Fields{
+		"Email":           validate.Rules(validate.Email),
+		"Password":        validate.Rules(validate.Password),
+		"ConfirmPassword": validate.Rules(validate.Equal(params.Password), validate.Message("passwords do not match")),
+	}).Validate(&errors); !ok {
+		return render(r, w, auth.SignupForm(params, errors))
+	}
+	user, err := sb.Client.Auth.SignUp(r.Context(), supabase.UserCredentials{
+		Email:    params.Email,
+		Password: params.Password,
+	})
+	if err != nil {
+		return err
+	}
+	return render(r, w, auth.SignupSuccess(user.Email))
 }
 
 func HandleLoginCreate(w http.ResponseWriter, r *http.Request) error {
 	credentials := supabase.UserCredentials{
 		Email:    r.FormValue("email"),
 		Password: r.FormValue("password"),
-	}
-
-	if !util.IsValidEmail(credentials.Email) {
-		return render(r, w, auth.LoginForm(credentials, auth.LoginErrors{
-			Email: "please enter a valid email address",
-		}))
-	}
-
-	if reason, ok := util.ValidatePassword(credentials.Password); !ok {
-		return render(r, w, auth.LoginForm(credentials, auth.LoginErrors{
-			Password: reason,
-		}))
 	}
 
 	resp, err := sb.Client.Auth.SignIn(r.Context(), credentials)
